04faa4e2d6be67155867dfdf0f5716ee23ab44d1
sympsonite <62749529+sympsonite@users.noreply.github.com>
2023-05-09 15:50:51 +0000

    changes

---
+++ test2.js
@@ -0,0 +1,7 @@
+const shopping = ['bread', 'milk', 'cheese', 'hummus', 'noodles'];
+console.log(shopping);
+console.log(shopping.length);
+console.log(shopping[0]); // returns "bread"
+
+shopping[0] = 'tahini';
+console.log(shopping);
